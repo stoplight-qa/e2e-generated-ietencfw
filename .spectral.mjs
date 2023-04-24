70bbde39e2141d8380081c7d96df3c6c7b00eae7
qa-nkyuh-0-xbbm@example.com <qa-nkyuh-0-xbbm@example.com>
2023-04-24 17:47:13 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16144.stoplight-dev.com/api/v1/projects/cHJqOjM0NQ/spectral.js?branch_slug=main&token=5de1fdba-ea13-4f1e-8373-a0e84073dee6";
+export default { extends: ruleset };
